fcca59d328ed701e5fe67879b392561ff297bd8e
François-Xavier MAURICARD <fxmauricard@gmail.com>
2017-06-08 16:14:28 +0200

    Simplified Startup.cs routing definition + adding context and service in DI.

--- Startup.cs
+++ Startup.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using UrlShortener.Data;
+using UrlShortener.Services;
 
 namespace UrlShortener
 {
@@ -29,6 +32,8 @@ namespace UrlShortener
         {
             // Add framework services.
             services.AddMvc();
+            services.AddDbContext<UrlShortenerContext>(options => options.UseSqlite("filename=shorturls.db"));
+            services.AddScoped<IShortUrlService, ShortUrlService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,12 +54,7 @@ namespace UrlShortener
 
             app.UseStaticFiles();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
