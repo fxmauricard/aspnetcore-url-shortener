c8563e78c8284e7ac578c24e841a400b5d3089ed
François-Xavier MAURICARD <fxmauricard@gmail.com>
2017-06-09 13:47:59 +0200

    Updated UI of the application, and removing some unused view/actions.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -20,13 +20,6 @@ namespace UrlShortener.Controllers
             return View();
         }
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
         public IActionResult Error()
         {
             return View();
--- Controllers/ShortUrlsController.cs
+++ Controllers/ShortUrlsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
 using UrlShortener.Helpers;
 using UrlShortener.Models;
 using UrlShortener.Services;
