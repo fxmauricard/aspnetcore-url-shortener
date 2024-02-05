f897111b1be07193a8550b2d4ab15d876647455e
alireza barkhordari <alirezab.1990@gmail.com>
2024-02-05 10:53:26 +0330

    update project to .net core 8

--- Program.cs
+++ Program.cs
@@ -1,10 +1,62 @@
-﻿using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
+using UrlShortener.Data;
+using UrlShortener.Services;
+using Microsoft.EntityFrameworkCore;
 
 namespace UrlShortener
 {
     public class Program
     {
+
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+
+            //
+
+            builder.Services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            builder.Services.AddDbContext<UrlShortenerContext>(options => options.UseSqlite("filename=shorturls.db"));
+            builder.Services.AddScoped<IShortUrlService, ShortUrlService>();
+
+            //
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+        /*
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
@@ -12,6 +64,6 @@ namespace UrlShortener
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>();*/
     }
 }
